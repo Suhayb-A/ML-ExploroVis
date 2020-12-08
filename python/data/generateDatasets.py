7a5ed52ac425f644e25fdcd9be659e404ea82d4e
MLivanos <prodigiousmike@gmail.com>
2020-12-07 16:36:30 -0800

    Add: generate datasets script in case more datasets are desired

---
+++ python/data/generateDatasets.py
@@ -0,0 +1,74 @@
+# This code is based on https://scikit-learn.org/stable/auto_examples/cluster/plot_cluster_comparison.html
+# but contains changes
+
+import time
+import warnings
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import cluster, datasets, mixture
+from sklearn.neighbors import kneighbors_graph
+from sklearn.preprocessing import StandardScaler
+from itertools import cycle, islice
+
+np.random.seed(0)
+
+n_samples = 1500
+noisy_circles = datasets.make_circles(n_samples=n_samples, factor=.5,
+                                      noise=.05)
+noisy_moons = datasets.make_moons(n_samples=n_samples, noise=.05)
+blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
+no_structure = np.random.rand(n_samples, 2), None
+
+# Anisotropicly distributed data
+random_state = 170
+X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
+transformation = [[0.6, -0.6], [-0.4, 0.8]]
+X_aniso = np.dot(X, transformation)
+aniso = (X_aniso, y)
+
+# blobs with varied variances
+varied = datasets.make_blobs(n_samples=n_samples,
+                             cluster_std=[1.0, 2.5, 0.5],
+                             random_state=random_state)
+
+default_base = {'quantile': .3,
+                'eps': .3,
+                'damping': .9,
+                'preference': -200,
+                'n_neighbors': 10,
+                'n_clusters': 3,
+                'min_samples': 20,
+                'xi': 0.05,
+                'min_cluster_size': 0.1}
+
+datasets = [
+    (noisy_circles, {'damping': .77, 'preference': -240,
+                     'quantile': .2, 'n_clusters': 2,
+                     'min_samples': 20, 'xi': 0.25}),
+    (noisy_moons, {'damping': .75, 'preference': -220, 'n_clusters': 2}),
+    (varied, {'eps': .18, 'n_neighbors': 2,
+              'min_samples': 5, 'xi': 0.035, 'min_cluster_size': .2}),
+    (aniso, {'eps': .15, 'n_neighbors': 2,
+             'min_samples': 20, 'xi': 0.1, 'min_cluster_size': .2}),
+    (blobs, {}),
+    (no_structure, {})]
+
+datasetNames = ['Circles', 'Moons', 'Varied', 'Linear', 'Globular', 'Uniform']
+
+for i_dataset, (dataset, algo_params) in enumerate(datasets):
+
+    X, y = dataset
+    datasetName = datasetNames[i_dataset]
+    if y is None:
+        print(datasetName)
+        y = np.zeros((X.shape[0]))
+    # normalize dataset for easier parameter selection
+    X = StandardScaler().fit_transform(X)
+    toCSV = np.zeros((X.shape[0], 3))
+    for i in range((X.shape[0])):
+    	toCSV[i,0] = X[i,0]
+    	toCSV[i,1] = X[i,1]
+    	toCSV[i,2] = y[i]
+    np.savetxt(datasetName + ".csv", toCSV, delimiter=",")
